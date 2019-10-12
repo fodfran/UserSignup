bcff3e956000913a38d6ecde451e0f29d72dba1d
Francesca F <fodfran@gmail.com>
2019-10-12 15:50:40 -0400

    finished part 2

--- UserSignup/Controllers/UserController.cs
+++ UserSignup/Controllers/UserController.cs
@@ -5,6 +5,7 @@ using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using UserSignup.Models;
+using UserSignup.ViewModels;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -12,95 +13,44 @@ namespace UserSignup.Controllers
 {
     public class UserController : Controller
     {
-        //static private List<User> Users = new List<User>();
-        //static private string error;
-
-        static private string usernameError;
-        static private string emailError;
-        static private string verifyError;
-        static private string passwordError;
-
-        static private string validUsername;
-        static private string validEmail;
-
+        
         // GET: /<controller>/
         public IActionResult Index()
         {
-            ViewBag.Users = UserData.GetAll();
-            return View();
+            List<User> Users = UserData.GetAll();
+            return View(Users);
         }
 
         public IActionResult Add()
         {
-            ViewBag.usernameError = usernameError;
-            usernameError = "";
-            ViewBag.emailError = emailError;
-            emailError = "";
-            ViewBag.passwordError = passwordError;
-            passwordError = "";
-            ViewBag.verifyError = verifyError;
-            verifyError = "";
+            AddUserViewModel addUserViewModel = new AddUserViewModel();
 
-            ViewBag.username = validUsername;
-            validUsername = "";
-            ViewBag.email = validEmail;
-            validEmail = "";
-            return View();
+            return View(addUserViewModel);
         }
 
         [HttpPost]
-        [Route("/User/Add")]
-        public IActionResult NewUser(User newUser, string verify)
+        public IActionResult Add(AddUserViewModel addUserViewModel)
         {
-            //Add new cheese to existing cheeses
-            validUsername = newUser.Username;
-            validEmail = newUser.Email;
-
-            Regex rgx = new Regex(@"^\w{5,15}$");
-
-            if (newUser.Username == null || rgx.IsMatch(newUser.Username) == false)
+            if (ModelState.IsValid)
             {
-                usernameError = "Invalid Username";
-                validUsername = "";
-                
-            }
-
-            if (newUser.Email == null)
-            {
-                emailError = "Invalid Email";
-                validEmail = "";
-            }
-
-            if (newUser.Password == null || rgx.IsMatch(newUser.Password) == false)
-            {
-                passwordError = "Invalid Password";
-                
-            }
-
-            if (verify == null || newUser.Password != verify)
-            {
-                verifyError = "Passwords don't match";
-                
-            }
-
-            if(usernameError=="" && emailError == "" && passwordError == "" && verifyError == "")
-            {
+                User newUser = AddUserViewModel.CreateUser(
+                    addUserViewModel.Username,
+                    addUserViewModel.Email,
+                    addUserViewModel.Password);
                 UserData.AddUser(newUser);
-                validUsername = "";
-                validEmail = "";
+
                 return Redirect("/User");
-                
             }
 
-            return Redirect("/User/Add");
+            return View(addUserViewModel);
 
         }
 
         public IActionResult Info(int userId)
         {
             
-            ViewBag.user = UserData.GetByID(userId);
-            return View();
+            User user = UserData.GetByID(userId);
+            return View(user);
         }
 
     }
